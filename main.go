b4f6395d4bb2ab9f2ca74ac396e96d1c43d3d6d5
Kody Eckheart <fozzbear123@gmail.com>
2025-02-08 20:17:53 -0800

    feat: Add /albums POST for single album

--- main.go
+++ main.go
@@ -15,9 +15,15 @@ type album struct {
 	Price  float64 `json:"price"`
 }
 
+var albums = []album{
+	{ID: "1", Title: "My Neck", Artist: "MyBack", Price: 3.99},
+	{ID: "2", Title: "Lick'm I. Puss E.", Artist: "Andmai Krak", Price: 4.99},
+}
+
 func main() {
 	router := gin.Default()
 	router.GET("/albums", getAlbums)
+	router.POST("/albums", postAlbums)
 
 	router.Run("localhost:8080")
 }
@@ -26,7 +32,16 @@ func getAlbums(c *gin.Context) {
 	c.IndentedJSON(http.StatusOK, albums)
 }
 
-var albums = []album{
-	{ID: "1", Title: "My Neck", Artist: "MyBack", Price: 3.99},
-	{ID: "2", Title: "Lick'm I. Puss E.", Artist: "Andmai Krak", Price: 4.99},
+// postAlbums adds an album from JSON received in the request body.
+func postAlbums(c *gin.Context) {
+	var newAlbum album
+
+	// Call BindJSON to bind the received JSON to newAlbum
+	if err := c.BindJSON(&newAlbum); err != nil {
+		return
+	}
+
+	// Add the new album to the slice.
+	albums = append(albums, newAlbum)
+	c.IndentedJSON(http.StatusCreated, newAlbum)
 }
