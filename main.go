3d7271a3d9b226b97cb4728cc51df74b934e7efa
Kody Eckheart <fozzbear123@gmail.com>
2025-02-08 20:45:23 -0800

    feat: Add /albums GET by id

--- main.go
+++ main.go
@@ -3,6 +3,7 @@
 package main
 
 import (
+	"fmt"
 	"net/http"
 
 	"github.com/gin-gonic/gin"
@@ -16,20 +17,36 @@ type album struct {
 }
 
 var albums = []album{
-	{ID: "1", Title: "My Neck", Artist: "MyBack", Price: 3.99},
-	{ID: "2", Title: "Lick'm I. Puss E.", Artist: "Andmai Krak", Price: 4.99},
+	{ID: "1", Title: "Adventures of Huckleberry Finn", Artist: "Mark Twain", Price: 3.99},
+	{ID: "2", Title: "The Martian", Artist: "Andy Weir", Price: 4.99},
 }
 
 func main() {
 	router := gin.Default()
 	router.GET("/albums", getAlbums)
+	router.GET("/albums/:id", getAlbumById)
 	router.POST("/albums", postAlbums)
 
+	fmt.Println("Starting")
 	router.Run("localhost:8080")
 }
 
 func getAlbums(c *gin.Context) {
 	c.IndentedJSON(http.StatusOK, albums)
+}
+
+// getAlbumById locates the album whose ID value matches the id parameter sent by the client, then returns
+// that album as a response.
+func getAlbumById(c *gin.Context) {
+	id := c.Param("id")
+
+	for _, a := range albums {
+		if a.ID == id {
+			c.IndentedJSON(http.StatusOK, a)
+			return
+		}
+	}
+	c.IndentedJSON(http.StatusNotFound, gin.H{"message": "album not found"})
 }
 
 // postAlbums adds an album from JSON received in the request body.
